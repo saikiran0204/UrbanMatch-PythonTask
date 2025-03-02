3e7f1da7bb84c4ab8a212ee16b79c49ea584d814
saikiran <saikiran0204@gmail.com>
2025-03-02 19:09:18 +0530

    Add proper api's

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, ARRAY
+from sqlalchemy import Column, Integer, String, Text, Boolean
+import json
 from database import Base
 
 class User(Base):
@@ -9,6 +10,25 @@ class User(Base):
     age = Column(Integer)
     gender = Column(String)
     email = Column(String, unique=True, index=True)
-    city = Column(String, index=True)
-    interests = Column(ARRAY(String))
+    city = Column(String)
+    interests = Column(Text)  # Store interests as a JSON string
+    is_active = Column(Boolean, default=True)
+
+    def __init__(self, name, age, gender, email, city, interests):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.email = email
+        self.city = city
+        self.interests = json.dumps(interests)  # Serialize list to JSON string
+        self.is_active = True
+
+    @property
+    def interests_list(self):
+        return json.loads(self.interests)  # Deserialize JSON string to list
+    
+
+    def model_dump(self):
+        self.interests = json.loads(self.interests) 
+        return self.__dict__
 
--- schemas.py
+++ schemas.py
@@ -1,20 +1,29 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, EmailStr, Field
+from typing import List, Optional
 
 class UserBase(BaseModel):
-    name: str
-    age: int
-    gender: str
-    email: str
-    city: str
-    interests: List[str]
+    name: str = Field(..., min_length=2, max_length=50, pattern=r"^[a-zA-Z\s]+$")
+    age: int = Field(..., ge=18, le=100)
+    gender: str = Field(..., pattern=r"^(male|female|other)$")
+    email: EmailStr  # Enforces valid email format
+    city: str = Field(..., pattern=r"^[a-zA-Z\s]+$")
+    interests: List[str] 
 
 class UserCreate(UserBase):
     pass
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    gender: Optional[str] = None
+    email: Optional[EmailStr] = None
+    city: Optional[str] = None
+    interests: Optional[List[str]] = None
+
 
 class User(UserBase):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
